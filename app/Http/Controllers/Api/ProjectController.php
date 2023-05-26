de5baed9930c7c3a4a5979e152fec89cdc0571b7
P4anix <pancok_1@yahoo.it>
2023-05-26 16:59:45 +0200

    creata rotta show su api più controllo per 404

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -18,4 +18,26 @@ class ProjectController extends Controller
         ]);
 
     }
+
+    public function show(string $slug){
+
+        $project = Project::where('slug', $slug)->with('type', 'technologies')->first();
+
+        if(!$project){
+
+            return response()->json([
+                'success' => true,
+                'result' => $project
+            ]);
+        } else {
+            return response()->json([
+                'success' => false,
+                'result' => null
+            ], 404);
+        }
+    }
+
+       
+
+    
 }
