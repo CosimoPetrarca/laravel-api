e2ce9a3ef47e073251fe537fe4e525df9476dfae
P4anix <pancok_1@yahoo.it>
2023-05-30 17:35:08 +0200

    migration e seeder di contact

--- database/migrations/2023_05_30_152115_create_contacts_table.php
+++ database/migrations/2023_05_30_152115_create_contacts_table.php
@@ -15,6 +15,9 @@ return new class extends Migration
     {
         Schema::create('contacts', function (Blueprint $table) {
             $table->id();
+            $table->string('name', 70);
+            $table->string('email', 70);
+            $table->text('message');
             $table->timestamps();
         });
     }
